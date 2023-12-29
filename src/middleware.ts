c02f754860e88921950183810179b1fe847bcfd9
javiparmu <mrjavistinson@gmail.com>
2023-12-29 03:18:35 +0100

    fix: secure next auth token

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,9 @@ import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest): Promise<NextResponse | void> {
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    const session = request.cookies.get('next-auth.session-token');
+    const session = request.cookies.get(
+      process.env.NODE_ENV === 'development' ? 'next-auth.session-token' : '__Secure-next-auth.session-token',
+    );
 
     if (!session) {
       return NextResponse.redirect(process.env.NEXTAUTH_URL ?? '/');
